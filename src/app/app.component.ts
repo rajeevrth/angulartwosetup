3a8828b0baeb7a908ef5d590414141cecf0a6734
rajeevrth <rajeevkumarsharma@outlook.com>
2017-03-30 13:26:29 +0530

    Initial Setup Angular2

---
+++ src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Hello {{name}}, {{World}}</h1>`,
+})
+export class AppComponent  { name = 'Angular'; World="Rajeev" }
